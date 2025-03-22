cf5a977ddce9b9c264d4c39ace0196b9d1bd63dd
hash5792 <202098574+hash5792@users.noreply.github.com>
2025-03-22 22:12:35 +0100

    feat: Add main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+// https://helderman.github.io/htpataic/htpataic01.html
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Welcome to Little Cave Adventure.\n")
+	fmt.Printf("It is very dark in here.\n")
+	fmt.Printf("\nBye!\n")
+}
